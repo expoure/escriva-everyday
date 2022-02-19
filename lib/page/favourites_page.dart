36c691e39466d6a3a2ff20c9990f2406562b7a24
Dyonatha Kramer <dyonatha.kramer@mksolutions.com.br>
2022-02-19 13:53:02 -0300

    Finalizar searchbar e atualizar lista de favoritos ao voltar do favorito detalhado

--- lib/page/favourites_page.dart
+++ lib/page/favourites_page.dart
@@ -1,7 +1,7 @@
 import 'package:escriva_everyday/models/Quote.dart';
 import 'package:escriva_everyday/page/favourites_page_detailed.dart';
 import 'package:escriva_everyday/utils/Database.dart';
-import 'package:escriva_everyday/widget/navigation_drawer_widget.dart';
+import 'package:escriva_everyday/widget/search_widget.dart';
 import 'package:flutter/material.dart';
 
 class FavouritesPage extends StatefulWidget {
@@ -10,13 +10,16 @@ class FavouritesPage extends StatefulWidget {
 }
 
 class _FavouritesPageState extends State<FavouritesPage> {
+  List<Quote> allQuotes = [];
   List<Quote> quotes = [];
+  String query = '';
 
   @override
   void initState() {
     super.initState();
     getQuotes().then((value) => {
           setState(() {
+            this.allQuotes = value;
             this.quotes = value;
           })
         });
@@ -27,6 +30,22 @@ class _FavouritesPageState extends State<FavouritesPage> {
     return quotes;
   }
 
+  void searchBook(String query) {
+    final quotes = this.allQuotes.where((quote) {
+      final chapterLower = quote.chapterName.toLowerCase();
+      final quoteLower = quote.quote.toLowerCase();
+      final bookLower = quote.bookName.toLowerCase();
+      return chapterLower.contains(query.toLowerCase()) ||
+          quoteLower.contains(query.toLowerCase()) ||
+          bookLower.contains(query.toLowerCase());
+    }).toList();
+    print(query);
+    setState(() {
+      this.query = query;
+      this.quotes = quotes;
+    });
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
       appBar: AppBar(
@@ -34,14 +53,23 @@ class _FavouritesPageState extends State<FavouritesPage> {
         backgroundColor: Color.fromRGBO(10, 30, 80, 1),
         actions: [],
       ),
-      body: ListView.builder(
-        padding: EdgeInsets.all(10),
-        itemCount: this.quotes.length,
-        itemBuilder: (context, index) {
-          print(this.quotes.length);
-          return this._quoteList(context, index);
-        },
+      body: Column(
+        children: [
+          buildSearch(),
+          Expanded(
+              child: ListView.builder(
+            padding: EdgeInsets.all(10),
+            itemCount: this.quotes.length,
+            itemBuilder: (context, index) {
+              print(this.quotes.length);
+              return this._quoteList(context, index);
+            },
+          )),
+        ],
       ));
+
+  Widget buildSearch() =>
+      SearchWidget(text: query, hintText: 'Pesquisar', onChanged: searchBook);
 
   _quoteList(BuildContext context, int index) {
     return GestureDetector(
@@ -70,13 +98,24 @@ class _FavouritesPageState extends State<FavouritesPage> {
 
   void showFavouriteQuoteDetail({required Quote quote}) {
     Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) => Scaffold(
-                appBar: AppBar(
-                  title: Text('Detalhe'),
-                  backgroundColor: Color.fromRGBO(10, 30, 80, 1),
-                ),
-                body: FavouritesPageDetailed(quote: quote))));
+            context,
+            MaterialPageRoute(
+                builder: (context) => Scaffold(
+                    appBar: AppBar(
+                      title: Text('Detalhe'),
+                      backgroundColor: Color.fromRGBO(10, 30, 80, 1),
+                    ),
+                    body: FavouritesPageDetailed(quote: quote))))
+        .then((value) => getQuotes().then((value) => {
+              setState(() {
+                this.allQuotes = value;
+                var stillBookmarked = this
+                    .allQuotes
+                    .where((quoteContext) => quoteContext.id == quote.id);
+                if (stillBookmarked.isEmpty) {
+                  this.quotes.removeWhere((value) => value.id == quote.id);
+                }
+              })
+            }));
   }
 }
